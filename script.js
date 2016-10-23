2b0ab5543d0086add07e85db9844320f33f69eda
R <rheechashaipu@gmail.com>
2016-10-23 19:23:49 -0400

    testing solutions to a browser problem occuring in mobile devices.
    address bars in mobiles causing site layout changes

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 $(document).ready(function() {
+  var hideAddressbar = function(){
+    document.body.style.minHeight = '1000px';
+    window.scrollTo(0, 0);
+    document.body.style.minHeight = window.innerHeight + 'px';
+}
+
+window.addEventListener('load', function() {
+    hideAddressbar();
+}, false);
+
+window.addEventListener('touchstart', hideAddressbar);
    var windowHeight = $(window).height();
    var menuPos = $('.nav-menu').position().top;
-    
+   var bg = $("body");
+   function resizeBackground(){
+    bg.height(windowHeight);
+   }
+   resizeBackground();
 
    $(window).scroll(function() {
        var scrollVal = $(this).scrollTop();
