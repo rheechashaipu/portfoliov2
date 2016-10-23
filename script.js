dc3a3893749e886c01b8f1a65c6e93ff61a8cd60
R <rheechashaipu@gmail.com>
2016-10-23 19:31:46 -0400

    got rid of the js solution attempt.

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
 $(document).ready(function() {
-  var hideAddressbar = function(){
-    document.body.style.minHeight = '1000px';
-    document.body.style.minHeight = window.innerHeight + 'px';
-}
-
-window.addEventListener('load', function() {
-    hideAddressbar();
-}, false);
-
-window.addEventListener('touchstart', hideAddressbar);
    var windowHeight = $(window).height();
    var menuPos = $('.nav-menu').position().top;
-   var bg = $("body");
-   function resizeBackground(){
-    bg.height(windowHeight);
-   }
-   resizeBackground();
+    
 
    $(window).scroll(function() {
        var scrollVal = $(this).scrollTop();
